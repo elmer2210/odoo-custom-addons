19236d8f90b7a9605fd3137c543d80957fccf809
Elmer Rivadeneira <elmerrivadeneira@gmail.com>
2026-02-24 00:21:37 -0500

    Corriendo bugs y rediseñando menus

--- crai_base/models/campus.py
+++ crai_base/models/campus.py
@@ -9,6 +9,14 @@ class CraiCampus(models.Model):
     code = fields.Char("Código", required=True, help="Código corto de la sede", translate=False)
     site_id = fields.Many2one('crai.site', string="Sede", required=True, ondelete='restrict', index=True)
     user_id = fields.Many2one('res.users', string="Responsable", ondelete="set null", index=True)
+    # 2. CAMPO NUEVO: La nueva relación Many2many
+    user_ids = fields.Many2many(
+        'res.users', 
+        relation='crai_campus_res_users_rel', 
+        column1='campus_id', 
+        column2='user_id_rel', # Le ponemos _rel para evitar choques con el campo user_id de arriba
+        string="Responsables"
+    )
     active = fields.Boolean(default=True)
 
     _sql_constraints = [
--- crai_base/models/res_users.py
+++ crai_base/models/res_users.py
@@ -3,6 +3,10 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    crai_campus_ids = fields.One2many(
-        "crai.campus", "user_id", string="Sedes CRAI (responsable)"
+    crai_campus_ids = fields.Many2many(
+        "crai.campus",
+        relation="crai_campus_res_users_rel",
+        column1="user_id_rel",
+        column2="campus_id",
+        string="Sedes CRAI (responsable)"
     )
